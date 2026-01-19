103f1eb04c25cbcef6150f714141ee9cf44e6d17
irem-yigit <yigitiremm@gmail.com>
2026-01-19 09:44:48 +0300

    feat: add first api hello

---
+++ README.md
@@ -0,0 +1,2 @@
+# Datateam Internship Projects 
+
--- backend/express/index.js
+++ backend/express/index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("Run express server");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 app.listen(3000, () => {
   console.log("Run server on port 3000");
 });
