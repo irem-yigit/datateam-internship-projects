cf3f938f756cad7f096075ad1d2ee56e09f9025d
irem-yigit <yigitiremm@gmail.com>
2026-01-20 10:56:09 +0300

    feat: create BookerApp web app and Model layer

---
+++ backend/BookerApp/Models/Book.cs
@@ -0,0 +1,14 @@
+namespace BookerApp.Models
+{
+    public class Book
+    {
+        public int Id {get; set;}
+        public String Title { get; set;}
+        public String Author { get; set;}
+        public String Publisher { get; set;}
+        public String Isbn { get;set;}
+        public int PageCount{ get;set;}
+        
+        
+    }
+}
---
+++ backend/BookerApp/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
