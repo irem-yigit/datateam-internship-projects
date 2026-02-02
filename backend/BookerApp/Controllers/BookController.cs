10cb68fba86db1a200df787630bfd7b05aef5a25
irem-yigit <yigitiremm@gmail.com>
2026-02-03 00:32:36 +0300

    feat: add controller layer in BookEntity

--- backend/BookerApp/Controllers/BookController.cs
+++ backend/BookerApp/Controllers/BookController.cs
@@ -11,11 +11,70 @@ namespace BookerApp.Controllers;
 [Route("api/books")]
 public class BookController : ControllerBase
 {
-    /*private readonly AppDbContext _context;
+    private readonly ApplicationDbContext _context;
 
-    public BookController(AppDbContext context)
+    public BookController(ApplicationDbContext context)
     {
         _context = context;
-    }*/
+    }
+
+    //Get all books
+    [HttpGet]
+    public IActionResult GetAllBooks()
+    {
+        var books = _context.Books.ToList();
+        return Ok(books);
+    }
+
+    //Get book by id
+    [HttpGet("{id}")]
+    public IActionResult GetBookById(int id)
+    {
+        var book = _context.Books.Find(id);
+        if (book == null)
+            return NotFound("Book not found");
+        return Ok(book);
+    }
+
+    //Add a new book
+    [HttpPost]
+    public IActionResult CreateBook ([FromBody] Book book)
+    {
+        _context.Books.Add(book);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetBookById),
+            new { id = book.Id },
+            book);
+    }
+
+    //Update a book
+    [HttpPut("{id}")]
+    public IActionResult UpdateBook(int id, [FromBody] Book book)
+    {
+        var existingBook = _context.Books.Find(id);
+        if (existingBook == null)
+            return NotFound("Book not found");
+
+        existingBook.Title = book.Title;
+        existingBook.Author = book.Author;
+        existingBook.ISBN = book.ISBN;
+        existingBook.PublicationYear = book.PublicationYear;
+
+        _context.SaveChanges();
+        return Ok(existingBook);
+    }
+    
+    //Delete a book by id
+    [HttpDelete("{id}")]    
+    public IActionResult DeleteBook(int id)
+    {
+        var book = _context.Books.Find(id);
+        if (book == null)
+            return NotFound("Book not found");
+
+        _context.Books.Remove(book);
+        _context.SaveChanges();
+        return NoContent();
+    }   
 
 }
